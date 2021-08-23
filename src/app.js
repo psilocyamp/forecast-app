2fedaf4f8cee61dd20c3543a3647e507f52ba48f
psilocyamp <ampienmonte@gmail.com>
2021-08-22 21:24:25 -0300

    put the search engine to work

--- src/app.js
+++ src/app.js
@@ -51,9 +51,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
+function search(city){
 let apiKey = "04d4f57a447d17fdbbb7030c83765030";
-let city = "buenos aires";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("buenos aires");
